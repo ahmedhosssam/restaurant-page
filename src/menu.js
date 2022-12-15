5cda3e92ea4781e9dde54c4c44c9d19baa4bfe4d
ahmedhossam <hoss4614@gmail.com>
2022-12-15 15:51:08 +0200

    finish adding elements & styling :  Home Page

--- src/menu.js
+++ src/menu.js
@@ -1,7 +1,28 @@
 export default function menuPage() {
   const menuPage = document.createElement('div');
   menuPage.classList.add('menu-page');
-  menuPage.innerText = 'M E N U !';
+  const createItem = (image, title, description) => {
+    const newItem = document.createElement('div');
+    newItem.classList.add('menu-item');
+
+    const itemTitle = document.createElement('h2');
+    itemTitle.textContent = title;
+
+    const itemDescription = document.createElement('p');
+    itemDescription.textContent = description;
+
+    newItem.appendChild(image);
+    newItem.appendChild(title);
+    newItem.appendChild(title);
+
+    menuPage.appendChild(newItem);
+  };
+
+  createItem(
+    '../assets/Burger1.png',
+    'Burger',
+    'A flame-grilled beef patty with juicy tomatoes, crisp lettuce, creamy mayonnaise, ketchup, crunchy pickles, and sliced white onions on a toasted sesame seed bun.'
+  );
 
   return menuPage;
 }
